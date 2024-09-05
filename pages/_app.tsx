9d0a8f7738e60df1c0e658469147e9746dfee9ab
Always Ritz <161363790+Ritz404@users.noreply.github.com>
2024-09-05 22:28:05 +0700

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,28 @@
+import '@/styles/globals.css'
+import { fontRubik } from '@/data/fonts'
+import type { AppProps } from 'next/app'
+import Headers from '@/components/Header';
+import Footer from '@/components/Footer';
+import Head from 'next/head';
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Always Ritz</title>
+        <meta name="description" content="Always Ritz's personal website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <style jsx global>{`
+        :root {
+          --rubik-font: ${fontRubik.style.fontFamily};
+        }
+      `}</style>
+      <Headers/>
+      <Component {...pageProps} />
+      <Footer/>
+    </>
+  );
+}
